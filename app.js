b89d73207f6ef60edbb1e0b36080c0b08b421ea8
AgustinM12 <agustinmazza12345@gmail.com>
2023-07-05 14:30:10 -0300

    se importaron las dependencias y se colocaron los middlewares en el archivo de arranque app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // Imports
-const cors = require('cors');
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+require('ejs')
+
+const port = process.env.PORT || 5000
 
 const path = require('path');
 
@@ -8,6 +14,16 @@ const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,4 +33,4 @@ app.use('/api', require('./routes/reserva.routes'));
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Servidor en el puerto${port}`));
